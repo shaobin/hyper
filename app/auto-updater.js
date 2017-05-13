9245edc5edb898dcf5dc601fc7c2a175d4fdf7c7
Bin Shao <binshao@live.com>
2017-05-13 14:13:59 +0800

    disables autoupdate

--- app/auto-updater.js
+++ app/auto-updater.js
@@ -1,4 +1,4 @@
-const {autoUpdater} = require('electron');
+﻿const {autoUpdater} = require('electron');
 const ms = require('ms');
 
 const notify = require('./notify'); // eslint-disable-line no-unused-vars
@@ -13,41 +13,8 @@ const FEED_URL = `https://hyper-updates.now.sh/update/${platform}`;
 let isInit = false;
 
 function init() {
-  autoUpdater.on('error', (err, msg) => {
-    console.error('Error fetching updates', msg + ' (' + err.stack + ')');
-  });
-
-  autoUpdater.setFeedURL(`${FEED_URL}/${version}`);
-
-  setTimeout(() => {
-    autoUpdater.checkForUpdates();
-  }, ms('10s'));
-
-  setInterval(() => {
-    autoUpdater.checkForUpdates();
-  }, ms('30m'));
-
   isInit = true;
 }
 
 module.exports = function (win) {
-  if (!isInit) {
-    init();
-  }
-
-  const {rpc} = win;
-
-  const onupdate = (ev, releaseNotes, releaseName) => {
-    rpc.emit('update available', {releaseNotes, releaseName});
-  };
-
-  autoUpdater.on('update-downloaded', onupdate);
-
-  rpc.once('quit and install', () => {
-    autoUpdater.quitAndInstall();
-  });
-
-  win.on('close', () => {
-    autoUpdater.removeListener('update-downloaded', onupdate);
-  });
 };
